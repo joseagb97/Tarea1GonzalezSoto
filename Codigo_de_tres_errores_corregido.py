e67c5f36b018482f6a1c5ba094f48c25c211cee9
joseagb97 <79766756+joseagb97@users.noreply.github.com>
2021-03-05 19:39:51 -0600

    Código con errores detectables por flake8 Fixed

---
+++ Codigo_de_tres_errores_corregido.py
@@ -0,0 +1,5 @@
+def suma_de_dos_numeros_enteros(x, y):
+    if x == type(int) and y == type(int):
+        print(x + y)        # Imprime el resultado de la suma
+    else:
+        print("Asegurese de que los numeros son enteros")
