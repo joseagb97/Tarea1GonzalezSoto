327d94b1e847615386311f275ebdeb65c0725b88
joseagb97 <79766756+joseagb97@users.noreply.github.com>
2021-03-05 19:37:17 -0600

    Código con errores detectables por flake8

---
+++ Codigo_de_tres_errores.py
@@ -0,0 +1,10 @@
+def suma_de_dos_numeros_enteros (x,y):
+    if x == type(int) and y == type(int):
+        print(x + y)        #Imprime el resultado de la suma
+    else:
+        print("Asegurese de que los numeros son enteros")
+
+# Errores
+# 1.En la primer linea los () de la función están separados del nombre.
+# 2.Las variables (x,y), la y no lleva espacion después de la coma.
+# 3.El comentario no tiene espacio después del "# aa".
